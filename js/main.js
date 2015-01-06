08bd010770076c906e4e655cf0ad72500f84a7c2
esandin <esandin@esriaustralia.com.au>
2015-01-06 18:09:34 +1100

    functional content in...well...functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 require(["esri/map", "dojo/domReady!"], function(Map) { 
-  var map = new Map("map", {
-    center: [149.1, -35.3],
-    zoom: 12,
-    basemap: "topo"
-  });
-  navigator.geolocation.getCurrentPosition(showPosition);
-  var coords = getElementById('coords');
-  coords.innerHTML = 'temp';
+  var map;
   function showPosition(position){
 	//coords.innerHTML = position.coords.latitude;
   };
   var init = function(){
-	  
+  map = new Map("map", {
+    center: [149.1, -35.3],
+    zoom: 12,
+    basemap: "topo"
+  });	  
+  };
+  var other = function(){
+  navigator.geolocation.getCurrentPosition(showPosition);
+  var coords = getElementById('coords');
+  coords.innerHTML = 'temp';	  
   };
   dojo.ready(init);
+  
 });
 
